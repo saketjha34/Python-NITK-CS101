555128429c06fa2696a17da71288d665d413553d
sakejha34 <saketjha0324@gmail.com>
2024-02-04 19:31:09 +0530

    Update sieve.py

--- A11 - A14/sieve.py
+++ A11 - A14/sieve.py
@@ -4,20 +4,15 @@ Sieve of Eratosthenes
  
 def sieve(limit):
     prime_list = []
-    if limit < 2:
+
+    if limit < 0:
         raise ValueError('Invalid input.')
 
     for i in range(2, limit):
-        n = 0 
-        if i == 1 or i == 0:
-            continue
+        for j in range(2, i):
+            if i % j == 0:
+                break
         else:
-            for j in range(2, i):
-                if i % j == 0:
-                    n = 1
-                    break
-            if n == 0:
-                prime_list.append(i)
+            prime_list.append(i)
     return prime_list
-
 
