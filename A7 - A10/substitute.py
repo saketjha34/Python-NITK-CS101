03ee7c04ed73048f4ffc74f16634117eb99b8fd0
sakejha34 <saketjha0324@gmail.com>
2024-02-04 19:17:18 +0530

    Update substitute.py

--- A7 - A10/substitute.py
+++ A7 - A10/substitute.py
@@ -3,10 +3,11 @@ Substitute letter
 '''
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def substitute(letter, n):
-    c = n - len(alphabets)
     for i in range(len(alphabets)):
         if alphabets[i] == letter:
             if n > 0:
                 return alphabets[(i+n) - (len(alphabets))]
             if n < 0:
                 return alphabets[(i+n)]
+            else:
+                return letter
